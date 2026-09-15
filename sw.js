7ac5ff10285e39b973761e27d199f922b9e7f8bc
james9612 <85613589+james9612@users.noreply.github.com>
2026-09-15 22:16:43 +0800

    com

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nfc-cpr-aed-v17';
+const CACHE_NAME = 'nfc-cpr-aed-v18';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -8,6 +8,7 @@ const ASSETS_TO_CACHE = [
   './js/sw-register.js',
   './manifest.webmanifest',
   './icons/icon.svg',
+  './images/org_emblem_trans.png',
   './images/aed_pads_guide.png',
   './images/aed_pads_guide.jpg',
   './audio/alert_119_aed.mp3'
